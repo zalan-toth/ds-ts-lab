6e819c3ab368d20e6f88a8dc7ab866a9131c5482
zalan-toth <zalant.tsc@gmail.com>
2025-09-22 22:16:39 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,6 +4,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    }
+}
+
 
 // (Optional) Change the names below to your friends.
 
@@ -23,7 +32,7 @@ const friends: Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -32,7 +41,7 @@ const colleague1 = {
   },
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -41,7 +50,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
