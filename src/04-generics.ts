e01b4b23cdd2f4f1c9066116daf400ed2bd6cc2a
zalan-toth <zalant.tsc@gmail.com>
2025-09-24 01:09:30 +0100

    Functions with generics.

---
+++ src/04-generics.ts
@@ -0,0 +1,24 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './myTypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+
+function sort<T>(data: T[], sorter: (a: T, b: T) => number): T[]{
+    return data.slice().sort(sorter)
+}
+console.log("-------")
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
